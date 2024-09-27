e21f21f24c9eee7186e422e430c9d19fa0741642
nicod12 <dumenicolas45@gmail.com>
2024-09-27 20:22:50 -0300

    upd

--- index.ts
+++ index.ts
@@ -1,19 +1,1 @@
-import { registro } from "./RegistroAutomotor";
 
-registro.setVehiculo("auto", "toyota", "corolla", 2019, "BUH123");
-registro.setVehiculo("moto", "Yamaha", "MT-09", 2018, "YUY456");
-registro.setVehiculo("camion", "Scania", "R 450", 2020, "IRI789");
-
-console.log(
-  "registro de vehiculo antes de actualizar",
-  registro.getVehiculos(),
-);
-
-registro.modificarVehiculo("BUH123", { modelo: "Corolla 2020", anio: 2020 });
-
-console.log("registro de vehiculo actualizado", registro.getVehiculos());
-
-registro.darDebaja("YUY456");
-
-console.log("Vehículos después de dar de baja:");
-console.log(registro.getVehiculos());
